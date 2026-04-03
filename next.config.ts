cd22c153f24c8fb0fa00b8780d0e4c6d334b994a
techDemons <nikhilnp417@gmail.com>
2026-04-03 19:32:46 +0530

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // ✅ required for Netlify manual deploy
+  // ✅ required for Netlify manual deploy
 
   images: {
     remotePatterns: [
